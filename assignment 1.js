ada7501a4f830fb96e3f997894f535afb89380a7
Felipe Dominguez <felipedominguez@felipes-MacBook-Pro.local>
2016-09-03 14:33:48 -0700

    First Assignment
    Using map, filter and reduce to control arrays of data such as objects.

---
+++ assignment 1.js
@@ -0,0 +1,63 @@
+//Problem #1
+
+var reviewsData = {
+  data: [
+    {
+      id: 1,
+      attributes: {
+        'review-title': 'Terrible!',
+        'stars': 1,
+        'cost': 'high'
+      }
+    },
+    {
+      id: 2,
+      attributes: {
+        'review-title': 'Decent place',
+        'stars': 3,
+        'cost': 'average'
+      }
+    },
+    {
+      id: 3,
+      attributes: {
+        'review-title': 'I would come again',
+        'stars': 3.5,
+        'cost': 'average'
+      }
+    },
+    {
+      id: 4,
+      attributes: {
+        'review-title': 'So good, and cheap!',
+        'stars': 4,
+        'cost': 'low-average'
+      }
+    },
+    {
+      id: 5,
+      attributes: {
+        'review-title': 'Good, but pricey!',
+        'stars': 4,
+        'cost': 'high'
+      }
+    }
+  ]
+};
+
+
+
+function extractReviews(reviewData) {
+  var extractIndividualReview = reviewsData.data.map(function(review){
+    
+    review.attributes.id = review.id;
+    return review.attributes
+    
+  });
+  return extractIndividualReview
+}
+
+console.log(extractReviews(reviewsData));
+
+
+//Problem #2 & #3
