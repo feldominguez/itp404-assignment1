f2d78be174f393ca2583d1bb1a65ff8c798d7f6b
Felipe Dominguez <felipedominguez@felipes-MacBook-Pro.local>
2016-09-03 17:26:31 -0700

    Problem #2 & #3
    Polishing of the last two problems

--- assignment 1.js
+++ assignment 1.js
@@ -60,4 +60,48 @@ function extractReviews(reviewData) {
 console.log(extractReviews(reviewsData));
 
 
-//Problem #2 & #3
+//Problem #2
+
+var reviews = [
+  { id: 1, title: 'Terrible!', stars: 1, cost: 'high' },
+  { id: 2, title: 'Decent place', stars: 3, cost: 'average' },
+  { id: 3, title: 'I would come again', stars: 3.5, cost: 'average' },
+  { id: 4, title: 'So good, and cheap!', stars: 4, cost: 'low-average' },
+  { id: 5, title: 'Good, but pricey!', stars: 4, cost: 'high' }
+];
+
+
+ function findGoodReviews(reviews) {
+   var goodReviews = reviews.filter(function(review){
+   return review.title.toLowerCase().indexOf('good') !== -1
+ });
+   return goodReviews
+ }
+
+ console.log(findGoodReviews(reviews));
+
+//Problem #3
+
+var reviews = [
+  { id: 1, title: 'Terrible!', stars: 1, cost: 'high' },
+  { id: 2, title: 'Decent place', stars: 3, cost: 'average' },
+  { id: 3, title: 'I would come again', stars: 3.5, cost: 'average' },
+  { id: 4, title: 'So good, and cheap!', stars: 4, cost: 'low-average' },
+  { id: 5, title: 'Good, but pricey!', stars: 4, cost: 'high' }
+];
+
+
+ function classifyReviewsByCost(reviews) {
+   
+   var reviewsByCost = reviews.reduce(function(iterating, review){
+    
+    var reviewCost = review.cost;
+    iterating.hasOwnProperty(reviewCost)? iterating[reviewCost]++ : iterating[reviewCost] = 1;
+   
+    return iterating;  
+   },{});
+  
+   return reviewsByCost
+ }
+
+ console.log(classifyReviewsByCost(reviews));
